4576d0f02bc49253d4cd51c3f0e64af152507e7a
jarkkokauppinen <jarkkokauppinen77@gmail.com>
2022-09-30 14:25:50 +0300

    Enable CORS

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 using mymovieswebapi;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    policy  =>
+    {
+        policy.WithOrigins(System.Environment.GetEnvironmentVariable("CLOUD_URL"));
+    });
+});
 
 // Add services to the container.
 
@@ -21,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
